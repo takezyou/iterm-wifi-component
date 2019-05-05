ac7d135c5f8b35f3be13599b0631cb63365e943c
geoff meyers <geoff@meye.rs>
2019-05-05 03:23:53 -0700

    switched to using SNR over rssi, adjusted regex to allow spaces (for ssid)

--- wifi_status/wifi_status.py
+++ wifi_status/wifi_status.py
@@ -2,7 +2,6 @@
 
 import iterm2
 import subprocess
-import math
 import re
 
 airport_path = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
@@ -24,20 +23,22 @@ async def main(connection):
         signal = ""
 
         output = subprocess.check_output(args=[airport_path, "-I"]).decode()
-        info = re.findall("[\s]*(agrCtlRSSI|state|lastTxRate|SSID):[\s]([!-~]*)", output)
-        rssi = info[0][1]
-        rate = info[2][1]
-        ssid = info[4][1]
+        info = re.findall("[\s]*(agrCtlRSSI|agrCtlNoise|state|lastTxRate|SSID):[\s]([!-~\ ]*)", output)
+        rssi = int(info[0][1])
+        noise = int(info[1][1])
+        rate = info[3][1]
+        ssid = info[5][1]
+        snr = rssi - noise
 
         if int(rate) == 0:
             signal = "🙅‍♂️"
             status = "{0}".format(signal)
         else:
-            _rssi = (5 - int(rssi) / -20)
-
-            for r in range(math.ceil(_rssi)):
-                signal = signal + signals[r]
+            # 25dB SNR -> 5 bars
+            power = min(snr // 5, 5)
+            signal = "".join(signals[:power])
             status = "{0}  | {1} |".format(ssid, signal)
+        # print(f"rssi: {rssi}, noise: {noise}, snr: {snr}, ssid: {ssid}")
         return status
 
     await component.async_register(connection, wifi_status)
